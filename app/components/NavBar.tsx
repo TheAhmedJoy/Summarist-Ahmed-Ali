f9fde640b7fcbf8b0d9d8564d3b24651209176ff
Ahmed Ali <theahmedjoyali@gmail.com>
2026-01-17 15:18:14 -0800

    NavBar, SearchBar, and Footer created

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,81 @@
+"use client"
+
+import { useDispatch } from "react-redux"
+import { usePathname } from "next/navigation"
+import Image from "next/image"
+import Link from "next/link"
+import { signOut } from "firebase/auth"
+import { auth } from "@/firebase/init"
+import { openLogin } from "../Redux/AuthenticationModalSlice"
+import { useAuth } from "../Provideers/AuthenticationProvider"
+import SearchBar from "./SearchBar"
+import Logo from "../../public/logo.png"
+
+
+export default function NavBar() {
+
+  const { user, loading } = useAuth()
+
+  const dispatch = useDispatch()
+
+  const pathname = usePathname()
+
+  if (pathname === "/choose-plan") {
+    return null
+  }
+
+  const appRoutes = ["/for-you", "/library", "/settings"]
+  const prefixRoutes = ["/book/"]
+  const isAppRoute = appRoutes.includes(pathname)
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth)
+    } catch (error) { }
+  }
+
+  return (
+    <div>
+      <nav className="nav">
+        <div className="nav__wrapper">
+          {isAppRoute ||
+            prefixRoutes.some((prefix) => pathname.startsWith(prefix)) ?
+            (
+              <div className="nav__search">
+                <SearchBar />
+                <div className="search__icon">
+                  <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0 0 11.6 0l43.6-43.5a8.2 8.2 0 0 0 0-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"></path></svg>
+                </div>
+              </div>
+            ) : (
+              <>
+                <Link href="/" className="nav__img--mask">
+                  <Image className="nav__img" src={Logo} alt="logo" />
+                </Link>
+                <ul className="nav__list--wrapper">
+                  {!loading && !user && (
+                    <li className="nav__list nav__list--login" onClick={() => dispatch(openLogin())}>
+                      Login
+                    </li>
+                  )}
+
+                  {!loading && user && (
+                    <li className="nav__list nav__list--user">
+                      <Link href={"for-you"}>
+                        <span className="nav__user-name">My Account</span>
+                      </Link>
+                      <button onClick={handleLogout}>logout</button>
+                    </li>
+                  )}
+
+                  <li className="nav__list nav__list--mobile">About</li>
+                  <li className="nav__list nav__list--mobile">Contact</li>
+                  <li className="nav__list nav__list--mobile">Help</li>
+                </ul>
+              </>
+            )}
+        </div>
+      </nav>
+    </div>
+  )
+}
